6ffe134827d647d465954dbb5415fa8553f4883e
BhavyaKhyalia <150340060+BhavyaKhyalia@users.noreply.github.com>
2024-06-22 19:56:19 +0530

    Add files via upload

---
+++ veiwPage.php
@@ -0,0 +1,53 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/> -->
+    <link rel="stylesheet" href="style.css" />
+    <title>Home Page</title>
+  </head>
+  <body>
+    <div class="header">
+      <img src="drdo-logo.png" alt="DRDO Logo">
+      <h1>Centre for Fire, Explosive and Environment Safety (CFEES)<br>
+          अग्नि, विस्फोटक और पर्यावरण सुरक्षा केंद्र (सीएफईईएस)
+      </h1>
+      <img src="drdo-logo.png" alt="DRDO Logo">
+  </div>
+    <div class="container">
+        <div class="box" id="box1" >
+            Antivirus
+        </div>
+        <div class="box" id="box2">
+            Windows OS Security
+        </div>
+        <div class="box" id="box3">
+            Ubuntu OS Security
+        </div>
+        <div class="box" id="box4">
+            IT Hardware Security
+        </div>
+        <div class="box" id="box5">
+            IT Software Security
+        </div>
+        <div class="box" id="box6">
+            IT audit
+        </div>
+        <div class="box" id="box7">
+            Others 
+        </div>
+        
+    </div>
+    <div class="upload"><a href="upload.html"><button class="btn"> Upload</button></a></div>
+    <footer>
+    <h2>Designed and maintained by QRS&IT group.</h2>
+    <div class="logout">
+        <ul>
+          <li><a href="logout.php">logout</a></li>
+          <li><a href="logout.php">back</a></li>
+        </ul>
+      </div>
+  </footer>
+    <script src="script.js"></script>
+  </body>
+</html>
